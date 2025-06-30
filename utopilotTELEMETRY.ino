6db3a2f167a536569cc2a79a58d34f56be5d2481
alsdroppers123 <107935434+alsdroppers123@users.noreply.github.com>
2025-06-30 22:33:41 +0545

    Update utopilotTELEMETRY.ino

--- utopilotTELEMETRY.ino
+++ utopilotTELEMETRY.ino
@@ -14,16 +14,17 @@
 #define RIGHT_AILERON_SERVO_PIN 8
 #define LEFT_AILERON_SERVO_PIN 9
 #define ELEVATOR_SERVO_PIN 10
+#define RUDDER 1
 #define ESC1_PIN 0
-#define ESC2_PIN 1
+#define ESC2_PIN 3
 #define GPS_RX 20
 #define GPS_TX 21
 #define PPM_FRAME_GAP 3000
 #define CALIBRATION_SAMPLES 500
 
 // WiFi & MQTT
-const char* ssid = "telemetry";
-const char* password = "telemetry";
+const char* ssid = "abhishekrijal_2.4";
+const char* password = "JWDLY2O936KDK4%";
 const char* mqtt_server = "test.mosquitto.org";
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
@@ -32,21 +33,27 @@ PubSubClient mqttClient(espClient);
 MPU6050 mpu;
 Adafruit_BMP280 bmp;
 WebServer server(80);
-Servo esc1, esc2, servoRight, servoLeft, servoElevator;
+Servo esc1, esc2, servoRight, servoLeft, servoElevator, servoRudder;
 TinyGPSPlus gps;
 HardwareSerial GPS_Serial(1);
 
 // EEPROM configuration structure
 struct Config {
+  // PID constants
   float kP, kI, kD;
   float altitudeKP, altitudeKD, altitudePitchKP;
+  float rollP, rollI, rollD;
+  float pitchP, pitchI, pitchD;
+  float yawP, yawI, yawD;
+  
+  // Reversal settings
   bool reverseAileron, reverseElevator;
   bool reverseRightAileron, reverseLeftAileron, reverseElevatorServo;
 } config;
 
 #define EEPROM_SIZE sizeof(Config)
 
-// State
+// State variables
 volatile uint16_t ppmChannels[9] = {1500};
 volatile uint8_t ppmIndex = 0;
 volatile uint32_t lastPPMTime = 0;
@@ -60,17 +67,22 @@ bool altitudeHoldEngaged = false;
 int throttleSignal = 1000;
 
 // Sensor data
-float accelX, accelY, accelZ, gyroX, gyroY;
+float accelX, accelY, accelZ, gyroX, gyroY, gyroZ;
 float roll, pitch, dt;
 float lastAltitude = 0, currentAltitude = 0, targetAltitude = 0;
 float altitudeError = 0, altitudeCorrection = 0;
 float altitudeDerivative = 0, altitudePitchCorrection = 0;
 
+// PID state variables
+float rollErrorSum = 0, lastRollError = 0;
+float pitchErrorSum = 0, lastPitchError = 0;
+float yawErrorSum = 0, lastYawError = 0;
+
 // GPS data
 String gpsLatitude = "0.0";
 String gpsLongitude = "0.0";
 
-// Macros
+// Macros for channel access
 #define ch1Value ppmChannels[0]
 #define ch2Value ppmChannels[1]
 #define ch3Value ppmChannels[2]
@@ -78,7 +90,7 @@ String gpsLongitude = "0.0";
 #define ch7Value ppmChannels[6]
 #define ch9Value ppmChannels[4]
 
-// PPM ISR
+// PPM Interrupt Service Routine
 void IRAM_ATTR ppmISR() {
   uint32_t now = micros();
   uint32_t duration = now - lastPPMTime;
@@ -105,27 +117,31 @@ void calibrateMPU() {
   calibrated = true;
 }
 
-// EEPROM functions
+// Load configuration from EEPROM
 void loadEEPROM() {
   EEPROM.begin(EEPROM_SIZE);
   EEPROM.get(0, config);
   if (isnan(config.kP)) {
-    config.kP = 2.7;
-    config.kI = 0.0;
-    config.kD = 2.0;
-    config.altitudeKP = 2.0;
-    config.altitudeKD = 0.8;
-    config.altitudePitchKP = 1.5;
+    // Default PID values
+    config.kP = 2.7; config.kI = 0.0; config.kD = 2.0;
+    config.altitudeKP = 2.0; config.altitudeKD = 0.8; config.altitudePitchKP = 1.5;
+    config.rollP = 1.5; config.rollI = 0.0; config.rollD = 0.8;
+    config.pitchP = 1.5; config.pitchI = 0.0; config.pitchD = 0.8;
+    config.yawP = 1.0; config.yawI = 0.0; config.yawD = 0.5;
+    
+    // Default reversal settings
     config.reverseAileron = false;
     config.reverseElevator = false;
     config.reverseRightAileron = false;
     config.reverseLeftAileron = false;
     config.reverseElevatorServo = false;
+    
     EEPROM.put(0, config);
     EEPROM.commit();
   }
 }
 
+// Save configuration to EEPROM
 void saveEEPROM() {
   EEPROM.put(0, config);
   EEPROM.commit();
@@ -159,15 +175,30 @@ void handleRoot() {
     window.onload = fetchGPS;
   </script></head><body>
   <div class="section">
-    <h1>PID Settings</h1>
+    <h1>Stabilization PID Settings</h1>
     <form method='GET' action='/update'>
+      <h2>Roll PID</h2>
+      P:<input name='rollP' value=")rawliteral" + String(config.rollP) + R"rawliteral("><br>
+      I:<input name='rollI' value=")rawliteral" + String(config.rollI) + R"rawliteral("><br>
+      D:<input name='rollD' value=")rawliteral" + String(config.rollD) + R"rawliteral("><br>
+      
+      <h2>Pitch PID</h2>
+      P:<input name='pitchP' value=")rawliteral" + String(config.pitchP) + R"rawliteral("><br>
+      I:<input name='pitchI' value=")rawliteral" + String(config.pitchI) + R"rawliteral("><br>
+      D:<input name='pitchD' value=")rawliteral" + String(config.pitchD) + R"rawliteral("><br>
+      
+      <h2>Yaw PID</h2>
+      P:<input name='yawP' value=")rawliteral" + String(config.yawP) + R"rawliteral("><br>
+      I:<input name='yawI' value=")rawliteral" + String(config.yawI) + R"rawliteral("><br>
+      D:<input name='yawD' value=")rawliteral" + String(config.yawD) + R"rawliteral("><br>
+      
+      <h2>Altitude Hold PID</h2>
       kP:<input name='kP' value=")rawliteral" + String(config.kP) + R"rawliteral("><br>
       kI:<input name='kI' value=")rawliteral" + String(config.kI) + R"rawliteral("><br>
       kD:<input name='kD' value=")rawliteral" + String(config.kD) + R"rawliteral("><br>
       Altitude KP:<input name='altitudeKP' value=")rawliteral" + String(config.altitudeKP) + R"rawliteral("><br>
       Altitude KD:<input name='altitudeKD' value=")rawliteral" + String(config.altitudeKD) + R"rawliteral("><br>
-      Altitude Pitch KP:<input name='altitudePitchKP' value=")rawliteral" + String(config.altitudePitchKP) + R"rawliteral("><br>
-      <input type='submit' value='Update'>
+      <input type='submit' value='Update PID'>
     </form>
   </div>
 
@@ -210,7 +241,17 @@ void handleUpdate() {
   if (server.hasArg("kD")) config.kD = server.arg("kD").toFloat();
   if (server.hasArg("altitudeKP")) config.altitudeKP = server.arg("altitudeKP").toFloat();
   if (server.hasArg("altitudeKD")) config.altitudeKD = server.arg("altitudeKD").toFloat();
-  if (server.hasArg("altitudePitchKP")) config.altitudePitchKP = server.arg("altitudePitchKP").toFloat();
+  
+  if (server.hasArg("rollP")) config.rollP = server.arg("rollP").toFloat();
+  if (server.hasArg("rollI")) config.rollI = server.arg("rollI").toFloat();
+  if (server.hasArg("rollD")) config.rollD = server.arg("rollD").toFloat();
+  if (server.hasArg("pitchP")) config.pitchP = server.arg("pitchP").toFloat();
+  if (server.hasArg("pitchI")) config.pitchI = server.arg("pitchI").toFloat();
+  if (server.hasArg("pitchD")) config.pitchD = server.arg("pitchD").toFloat();
+  if (server.hasArg("yawP")) config.yawP = server.arg("yawP").toFloat();
+  if (server.hasArg("yawI")) config.yawI = server.arg("yawI").toFloat();
+  if (server.hasArg("yawD")) config.yawD = server.arg("yawD").toFloat();
+  
   saveEEPROM();
   server.send(200, "text/plain", "PID settings updated. <a href='/'>Back</a>");
 }
@@ -248,6 +289,8 @@ void checkWiFi() {
     Serial.println("Reconnecting WiFi...");
     WiFi.disconnect();
     WiFi.begin(ssid, password);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
     lastWiFiCheck = millis();
   }
 }
@@ -273,28 +316,53 @@ void setup() {
   EEPROM.begin(EEPROM_SIZE);
   loadEEPROM();
   
+  // Initialize MPU6050
   mpu.initialize();
-  if (!mpu.testConnection()) while (1);
-  calibrateMPU();
-  if (!bmp.begin(0x76)) while (1);
-
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 connection failed");
+    while (1);
+  }
+  
+  // Initialize BMP280
+  if (!bmp.begin(0x76)) {
+    Serial.println("BMP280 connection failed");
+    while (1);
+  }
+  
+  // Attach servos and ESCs
   esc1.attach(ESC1_PIN, 1000, 2000);
   esc2.attach(ESC2_PIN, 1000, 2000);
   servoRight.attach(RIGHT_AILERON_SERVO_PIN, 1000, 2000);
   servoLeft.attach(LEFT_AILERON_SERVO_PIN, 1000, 2000);
   servoElevator.attach(ELEVATOR_SERVO_PIN, 1000, 2000);
+  servoRudder.attach(RUDDER, 1000, 2000);
 
+  // Initialize motors to minimum throttle
   esc1.writeMicroseconds(1000);
   esc2.writeMicroseconds(1000);
 
+  // Setup PPM input
   pinMode(PPM_PIN, INPUT_PULLUP);
   attachInterrupt(PPM_PIN, ppmISR, RISING);
 
+  // Initialize GPS
   GPS_Serial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
 
+  // Connect to WiFi
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setup MQTT
   mqttClient.setServer(mqtt_server, 1883);
 
+  // Setup web server
   server.on("/", handleRoot);
   server.on("/update", handleUpdate);
   server.on("/reverse", handleReverse);
@@ -302,17 +370,22 @@ void setup() {
   server.on("/calibrate", handleCalibrate);
   server.on("/gps", handleGPS);
   server.begin();
+
+  // Calibrate IMU
+  calibrateMPU();
 }
 
 void loop() {
   static unsigned long lastTime = millis();
   static unsigned long lastMqttPublish = millis();
 
+  // Handle network connections
   checkWiFi();
   checkMQTT();
   mqttClient.loop();
   server.handleClient();
 
+  // Process GPS data
   while (GPS_Serial.available()) {
     gps.encode(GPS_Serial.read());
     if (gps.location.isUpdated()) {
@@ -321,7 +394,10 @@ void loop() {
     }
   }
 
+  // Read altitude
   currentAltitude = bmp.readAltitude(1013.25);
+  
+  // Check flight modes
   bool newAltHold = ch9Value >= 1800;
   if (newAltHold && !altitudeHoldEngaged) {
     targetAltitude = currentAltitude;
@@ -331,26 +407,72 @@ void loop() {
   gyroStabilizationEnabled = (ch9Value >= 1400);
   differentialThrustEnabled = (ch7Value >= 1500);
 
+  // Calculate time delta
   unsigned long now = millis();
   dt = (now - lastTime) / 1000.0;
   lastTime = now;
 
+  // Read IMU data
   accelX = mpu.getAccelerationX();
   accelY = mpu.getAccelerationY();
   accelZ = -mpu.getAccelerationZ();
-  gyroX = mpu.getRotationX() / 131.0;
+  gyroX = mpu.getRotationX() / 131.0;  // Convert to deg/s
   gyroY = mpu.getRotationY() / 131.0;
+  gyroZ = mpu.getRotationZ() / 131.0;
 
+  // Calculate attitude
   float accelRoll = atan2(-accelY, accelZ) * 180 / PI - rollOffset;
   float accelPitch = atan2(accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180 / PI - pitchOffset;
   rollFiltered = 0.98 * (rollFiltered + gyroX * dt) + 0.02 * accelRoll;
   pitchFiltered = 0.98 * (pitchFiltered + gyroY * dt) + 0.02 * accelPitch;
 
-  float rollError = -rollFiltered;
-  float pitchError = -pitchFiltered;
-  float rollCorrection = gyroStabilizationEnabled ? config.kP * rollError : 0;
-  float pitchCorrection = gyroStabilizationEnabled ? config.kP * pitchError : 0;
+  // Initialize target positions from RC input
+  int targetAileron = constrain(map(ch1Value, 1000, 2000, 0, 180), 0, 180);
+  int targetElevator = constrain(map(ch2Value, 1000, 2000, 0, 180), 0, 180);
+  int targetRudder = constrain(map(ch4Value, 1000, 2000, 0, 180), 0, 180);
 
+  // Apply gyro stabilization if enabled
+  if (gyroStabilizationEnabled) {
+    // Calculate errors
+    float rollError = -rollFiltered;  // Negative because we want to counteract the movement
+    float pitchError = -pitchFiltered;
+    float yawError = -gyroZ * dt;     // Yaw stabilization based on rate only
+
+    // Roll PID
+    rollErrorSum += rollError * dt;
+    float rollDerivative = (rollError - lastRollError) / dt;
+    float rollCorrection = config.rollP * rollError + config.rollI * rollErrorSum + config.rollD * rollDerivative;
+    lastRollError = rollError;
+
+    // Pitch PID
+    pitchErrorSum += pitchError * dt;
+    float pitchDerivative = (pitchError - lastPitchError) / dt;
+    float pitchCorrection = config.pitchP * pitchError + config.pitchI * pitchErrorSum + config.pitchD * pitchDerivative;
+    lastPitchError = pitchError;
+
+    // Yaw PID
+    yawErrorSum += yawError * dt;
+    float yawDerivative = (yawError - lastYawError) / dt;
+    float yawCorrection = config.yawP * yawError + config.yawI * yawErrorSum + config.yawD * yawDerivative;
+    lastYawError = yawError;
+
+    // Apply corrections
+    targetAileron += rollCorrection;
+    targetElevator += pitchCorrection;
+    targetRudder += yawCorrection;
+
+    // Constrain the outputs
+    targetAileron = constrain(targetAileron, 0, 180);
+    targetElevator = constrain(targetElevator, 0, 180);
+    targetRudder = constrain(targetRudder, 0, 180);
+
+    // Anti-windup - only integrate if not at limits
+    if (targetAileron <= 0 || targetAileron >= 180) rollErrorSum = 0;
+    if (targetElevator <= 0 || targetElevator >= 180) pitchErrorSum = 0;
+    if (targetRudder <= 0 || targetRudder >= 180) yawErrorSum = 0;
+  }
+
+  // Apply altitude hold if enabled
   if (altitudeHoldEnabled && altitudeHoldEngaged) {
     altitudeError = targetAltitude - currentAltitude;
     altitudeDerivative = (lastAltitude - currentAltitude) / dt;
@@ -362,12 +484,11 @@ void loop() {
     altitudePitchCorrection = 0;
   }
 
+  // Apply smoothing to control surfaces
   static float smoothAileron = 90, smoothElevator = 90;
   float alpha = 0.5;
-  int targetAileron = constrain(map(ch1Value, 1000, 2000, 0, 180) + rollCorrection, 0, 180);
-  int targetElevator = constrain(map(ch2Value, 1000, 2000, 0, 180) + pitchCorrection + altitudePitchCorrection, 0, 180);
   smoothAileron = alpha * targetAileron + (1 - alpha) * smoothAileron;
-  smoothElevator = alpha * targetElevator + (1 - alpha) * smoothElevator;
+  smoothElevator = alpha * (targetElevator + altitudePitchCorrection) + (1 - alpha) * smoothElevator;
   throttleSignal = constrain(ch3Value + altitudeCorrection, 1000, 2000);
 
   // Apply servo reversals
@@ -392,7 +513,11 @@ void loop() {
   } else {
     servoElevator.write(elevatorPos);
   }
+  
+  // Handle rudder
+  servoRudder.write(targetRudder);
 
+  // Handle differential thrust
   if (differentialThrustEnabled) {
     int yawOffset = map(ch4Value, 1000, 2000, -100, 100);
     int esc1Throttle = constrain(throttleSignal + yawOffset, 1000, 2000);
@@ -404,6 +529,7 @@ void loop() {
     esc2.writeMicroseconds(throttleSignal);
   }
 
+  // Publish telemetry
   if (millis() - lastMqttPublish > 500 && mqttClient.connected()) {
     String telemetryJson = "{";
     telemetryJson += "\"altitude\":" + String(currentAltitude, 2) + ",";
@@ -423,3 +549,6 @@ void loop() {
 
   delay(20);
 }
+
+
+
